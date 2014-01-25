e59235d38f8086ac7c02f95fa48944e144c2f6fa
malik <malik.boussejra@eleves.ec-nantes.fr>
2014-01-25 13:06:04 +0100

    Started to write filters

--- src/dictionary/Dico.java
+++ src/dictionary/Dico.java
@@ -4,26 +4,54 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Dico {
-	HashMap<String,Word> words;
-
+	HashMap<String,Word> wordsTrad;
+	HashMap<String,Word> wordsSimp;
+	Set<String> setTrad;
+	Set<String> setSimp;
+	
 	Dico() throws IOException{
-		words=new HashMap<String,Word>();
+		wordsTrad=new HashMap<String,Word>();
+		wordsSimp=new HashMap<String,Word>();
 		InputStreamReader reader = new InputStreamReader(new FileInputStream("cedict_ts.u8"),"UTF-8");
 		BufferedReader br = new BufferedReader(reader);
 		String line;
 		while((line=br.readLine())!=null){
 			if(line.charAt(0)!='#'){
-				String trad = line.substring(0, line.indexOf(' '));
-				words.put(trad, new Word(line));
+				int i = line.indexOf(' ');
+				int j = line.indexOf(' ',i+1);
+				String trad = line.substring(0, i);
+				Word w = new Word(line);
+				wordsTrad.put(trad, w);
+				String simp = line.substring(i+1, j);
+				wordsSimp.put(simp, w);
 			}
 		}
 		br.close();
+		setTrad=wordsTrad.keySet();
+		setSimp=wordsTrad.keySet();
+	}
+	
+	Set<Word> findWordsMatching(String filter){
+		Set<Word> result = new HashSet<Word>();
+		for(String word : this.setSimp){
+			if(Dico.match(word,filter))
+				result.add(wordsSimp.get(word));
+		}
+		return result;
 	}
 
+	private static boolean match(String word, String filter){
+		//NOTHING DONE
+		return false;
+	}
+	
 	public static void main(String[] args) throws IOException{
 		long tic = Calendar.getInstance().getTimeInMillis();
 		Dico dic = new Dico();
